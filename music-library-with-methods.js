88ea6fa1daae34353babf1fbeff8263747d79885
DevYves <yfreypons@gmail.com>
2017-12-01 21:51:58 +0000

    created simple class structure schema

---
+++ music-library-with-methods.js
@@ -0,0 +1,35 @@
+// A Library has a name and a creator (both strings)
+
+class Library(){
+  constructor() {
+    this.name = "";
+    this.creator = "";
+    this.playlist =[];
+    this.tracks= [];
+  }
+}
+
+
+
+class playlist() {
+  constructor() {
+    this.name = "";
+    this.tracks = [];
+  }
+}
+
+class tracks() {
+  constructor() {
+    this.name =""
+    this.rating =[];
+    this.length=[];
+  }
+}
+
+
+
+
+
+//playlists can be created as an instance of a library - use new keyword to create instance
+
+
